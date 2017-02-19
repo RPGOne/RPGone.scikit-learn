f35fe24e3e0f8e7dbc67581bb89e8a759e139e66
RPGOne <rosenraychov9@gmail.com>
2017-02-19 16:06:50 +0200

    RTM

---
+++ Advanced-JS-Lab-Poppy/Skeleton/poppy/core.js
@@ -0,0 +1,15 @@
+function pop(type, title, message) {
+    var popup;
+    switch (type) {
+        // TODO: Implement various popup types
+    }
+	
+	// generate view from view factory
+    var view = createPopupView(popup);
+
+    processPopup(view, popup);
+}
+
+function processPopup(domView, popup) {
+	// TODO: Implement popup logic
+}
