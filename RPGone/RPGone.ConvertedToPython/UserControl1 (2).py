f74f0baa5f45f500541b20640876611a9a4e9df4
RPGOne <rosenraychov9@gmail.com>
2017-02-19 21:48:25 +0200

    RTM

---
+++ RPGone/RPGone.ConvertedToPython/UserControl1 (2).py
@@ -0,0 +1,24 @@
+﻿# 
+# * Created by SharpDevelop.
+# * User: Rosen
+# * Date: 7.8.2015 г.
+# * Time: 14:42 ч.
+# *
+# * To change this template use Tools | Options | Coding | Edit Standard Headers.
+# 
+from System import *
+from System.Collections.Generic import *
+from System.ComponentModel import *
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class UserControl1(UserControl):
+	""" <summary>
+	 Description of UserControl1.
+	 </summary>
+	"""
+	def __init__(self):
+		#
+		# The InitializeComponent() call is required for Windows Forms designer support.
+		#
+		self.InitializeComponent(Char.ConvertToUtf32.Parse(uni))
---
+++ RPGone/RPGone.ConvertedToPython/UserControl1.Designer.py
@@ -0,0 +1,38 @@
+﻿# 
+# * Created by SharpDevelop.
+# * User: Daniela
+# * Date: 7.8.2015 г.
+# * Time: 14:42 ч.
+# *
+# * To change this template use Tools | Options | Coding | Edit Standard Headers.
+# 
+class UserControl1(object):
+	def __init__(self):
+		# <summary>
+		# Designer variable used to keep track of non-visual components.
+		# </summary>
+		self._components = None
+
+	def Dispose(self, disposing):
+		""" <summary>
+		 Disposes resources used by the form.
+		 </summary>
+		 <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		"""
+		if disposing:
+			if self._components != None:
+				self._components.Dispose()
+		self.Dispose(disposing)
+
+	def InitializeComponent(self):
+		""" <summary>
+		 This method is required for Windows Forms designer support.
+		 Do not change the method contents inside the source code editor. The Forms designer might
+		 not be able to load this method if it was changed manually.
+		 </summary>
+		"""
+		# 
+		# UserControl1
+		# 
+		self._AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		self._Name = "UserControl1"
