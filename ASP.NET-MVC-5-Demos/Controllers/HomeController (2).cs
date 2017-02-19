4bef6adc51a0fade15b49efde98c8a3851e24637
RPGOne <rosenraychov9@gmail.com>
2017-02-19 17:21:13 +0200

    RTM

---
+++ ASP.NET-MVC-5-Demos/Controllers/HomeController (2).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ASP.NET_MVC_5_Demos.Services;
+
+namespace ASP.NET_MVC_5_Demos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICacheService cacheService;
+
+        public HomeController(ICacheService cacheService)
+        {
+            this.cacheService = cacheService;
+        }
+
+        public IActionResult Index()
+        {
+            this.ViewBag.Bookmarks = this.cacheService.Bookmars;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "My new application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
