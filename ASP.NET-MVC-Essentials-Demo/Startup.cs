3dde836c071a8c1753ea19fb65f518532fb6e463
RPGOne <rosenraychov9@gmail.com>
2017-02-27 10:08:15 +0200

    RTM

---
+++ ASP.NET-MVC-Essentials-Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC_Essentials_Demo.Startup))]
+namespace ASP.NET_MVC_Essentials_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
